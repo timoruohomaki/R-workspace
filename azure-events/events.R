5ab0f334078a1253839fa611ea5bade1bd503168
Timo Ruohomaki <timo@ruohomaki.fi>
2024-11-03 19:30:44 +0200

    blob connection completed

--- azure-events/events.R
+++ azure-events/events.R
@@ -1,6 +1,18 @@
 library(httr)
 library(jsonlite)
 library(dotenv)
+library(AzureStor)
 
-Sys.getenv("VERSION")
+# workaround to reload dotenv after adding new keys
+detach("package:dotenv", unload=TRUE)
+library(dotenv)
+
+# Connecting to BLOB Storage
+
+aBlob <- storage_endpoint(endpoint=Sys.getenv("AzureBlobUrl"), key=Sys.getenv("AzureKey1"))
+aCont <- storage_container(endpoint=aBlob, name=Sys.getenv("AzureContainer"))
+
+files <- list_storage_files(container = aCont)
+
+
 
